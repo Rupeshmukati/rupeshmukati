f66a228e67b3f528acfa7890e3e8a8c90d708efe
Rupeshmukati <33753207+Rupeshmukati@users.noreply.github.com>
2026-01-14 21:55:32 +0530

    Matched port with proxy and added frontend production path

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // ================= Server =================
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
